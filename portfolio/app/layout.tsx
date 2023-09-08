01d75d3012948674b05cec3b9b409f1d5d1480e1
Yayehyrad <yayehyrad.sisay@gmail.com>
2023-09-08 20:37:02 +0300

    Nav bar

--- portfolio/app/layout.tsx
+++ portfolio/app/layout.tsx
@@ -1,6 +1,9 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Nav from '@/components/Nav'
+import Header from "@/components/Header"
+import TopLeftImg  from "@/components/TopLeftImg"
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +19,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className='page bg-site text-white bg-cover bg-no-repeat relative'>
+        <Nav />
+        <Header />
+        <TopLeftImg />
+        {children}
+        </div>
+        </body>
     </html>
   )
 }
--- portfolio/components/Nav.js
+++ portfolio/components/Nav.js
@@ -1,3 +1,4 @@
+"use client"
 // icons
 import {
   HiHome,
@@ -25,9 +26,30 @@ export const navData = [
     icon: <HiEnvelope />,
   },
 ];
+import Link from 'next/link';
+import { useRouter , usePathname } from 'next/navigation';
+ 
 
 const Nav = () => {
-  return <nav>nav</nav>;
+  const router = useRouter()
+  const pathname = usePathname()
+  console.log(pathname)
+  return <nav 
+            className='flex flex-col   fixed h-max items-center xl:justify-center 
+             gap-y-4 bottom-0 xl:right-[2%] z-50 top-0 w-full mt-auto xl:w-16 xl:max-w-md xl:h-screen'> 
+    <div 
+    className='flex w-full justify-between items-center xl:flex-col 
+    xl:justify-center gap-y-10 px-4 md:px-40 xl:p-0 h-[80px] bg-white/10 xl:h-max xl:py-8
+    text-3xl backdrop-blur-md xl:rounded-full xl:text-xl'>
+    {
+      navData.map((link , index)=>{
+        return <Link href={link.path} key={index} className={`${link.path === pathname && 'text-red-700'}`}>
+          {link.icon}
+          </Link>
+      })
+    }
+    </div>
+  </nav>;
 };
 
 export default Nav;
