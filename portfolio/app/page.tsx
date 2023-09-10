4383ad9e2caa4c2e2f42084d4b6568cb199147a7
Yayehyrad <yayehyrad.sisay@gmail.com>
2023-09-11 00:13:41 +0300

    home

--- portfolio/app/page.tsx
+++ portfolio/app/page.tsx
@@ -1,9 +1,64 @@
 import Image from 'next/image'
+import ParticlesContainer from "@/components/ParticlesContainer"
+import ProjectsBtn from "@/components/ProjectsBtn"
+import Avatar from "@/components/Avatar"
+import {motion} from "framer-motion"
+
+const fadeIn = (direction, delay) => {
+  return {
+    hidden: {
+      y: direction === 'up' ? 80 : direction === 'down' ? -80 : 0,
+      opacity: 0,
+      x: direction === 'left' ? 80 : direction === 'right' ? -80 : 0,
+      transition: {
+        type: 'tween',
+        duration: 1.5,
+        delay: delay,
+        ease: [0.25, 0.6, 0.3, 0.8],
+      },
+    },
+    show: {
+      y: 0,
+      x: 0,
+      opacity: 1,
+      transition: {
+        type: 'tween',
+        duration: 1.4,
+        delay: delay,
+        ease: [0.25, 0.25, 0.25, 0.75],
+      },
+    },
+  };
+};
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     {/* <h1 className=' text-red-700'>Portfolio</h1> */}
+    <main className="">
+      <div className=' h-screen bg-primary/60'>
+        <div className=' w-full h-full bg-gradient-to-r from-primary/10 via-black/30 to-black/10'>
+          <div className=' text-center flex  flex-col justify-center xl:text-left xl:p-40  h-full container mx-auto'>
+            <h1 className='h1'>
+              Transforming Ideas 
+              <br /> 
+              In To 
+              <span className=' text-red-800 animate-pulse ml-2'>Digital reality</span>
+            </h1>
+            <p className=' max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16'>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Obcaecati iste repellat assumenda eius molestiae autem omnis 
+              fugit pariatur voluptate velit?
+            </p>
+            <div className='flex justify-center xl:hidden relative'>
+              <ProjectsBtn /> 
+            </div>
+            <div className='hidden xl:felx'>
+              <ProjectsBtn /> 
+            </div>
+          </div>
+        </div>
+
+      </div>
+       <h1 className=' text-red-700'>Portfolio</h1>  
     </main>
   )
 }
