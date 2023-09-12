6b00a683619a890b31e51048cd3c11ccc2b53e56
Yayehyrad <yayehyrad.sisay@gmail.com>
2023-09-12 22:32:04 +0300

    upt

--- portfolio/components/ParticlesContainer.js
+++ portfolio/components/ParticlesContainer.js
@@ -1,5 +1,67 @@
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
+
 const ParticlesContainer = () => {
-  return <div>Particles Container</div>;
+  const particlsInit = useCallback(
+    async(engin) => {
+   
+    },
+    [],
+  )
+  const particlesloded = useCallback(
+    async( ) => {
+   
+    },
+    [],
+  )
+
+  
+  return  <Particles className="w-full" id="tsparticles" init={particlsInit} loaded={particlesloded} 
+  options={{
+    fullScreen:{enable : false},
+    background: {
+      color : {
+        value : ""
+      }
+    },
+    fpsLimit : 120,
+    interactivity :{
+      events :{
+        onClick :{
+          enable : false ,
+          mode : 'push'
+        },
+        onHover :{
+          enable : true ,
+          mode : 'repulse'
+        },
+        resize:true
+      },
+      modes :{
+        push : {
+          quantity:90
+        },
+        replse :{
+          distance : 200,
+          duration : 0.4
+        }
+      }
+    },
+    particles:{
+      color : {
+        value :"#e68e2e"
+      },
+      links : {
+        color : "#f5d393",
+        distance : 150,
+        enable : true ,
+        opacity :0.5,
+        width : 1
+      }
+    }
+    
+    
+  }}/>
 };
 
 export default ParticlesContainer;
