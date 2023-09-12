59fc1660934b2fade32e1ccade6f57b96984caae
Yayehyrad <yayehyrad.sisay@gmail.com>
2023-09-12 22:34:10 +0300

    upt

--- portfolio/components/ParticlesContainer.js
+++ portfolio/components/ParticlesContainer.js
@@ -57,6 +57,16 @@ const ParticlesContainer = () => {
         enable : true ,
         opacity :0.5,
         width : 1
+      },
+      collisions:{
+        enable : true
+      },
+      move:{
+        direction:'none',
+        enable : true,
+        outModes : {
+          default : "bounce"
+        }
       }
     }
     
