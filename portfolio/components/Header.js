0eb60e185530285f4ceb838eb75e317d39a2cdb5
Yayehyrad <yayehyrad.sisay@gmail.com>
2023-09-13 21:42:09 +0300

    logo

--- portfolio/components/Header.js
+++ portfolio/components/Header.js
@@ -1,5 +1,18 @@
+import Image from "next/image";
+import Link from "next/link";
+import Socials from "@/components/Socials"
 const Header = () => {
-  return <header className="z-10  w-full absolute">Header</header>;
+  return <header className="z-30  w-full absolute flex items-center px-16 xl:px-0 xl:h-[90px]">
+    <div className="container mx-auto "> 
+      <div className="flex">
+        <Link href={"/"}>
+          <Image src={"/logo.svg"} width={220} height={48} alt="" priority={true}/>
+        </Link>
+        <Socials />
+      </div>
+    </div>
+  
+    </header>;
 };
 
 export default Header;
