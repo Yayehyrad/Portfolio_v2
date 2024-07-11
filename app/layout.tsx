db23d2dcab368845057b2f9683fcf47439131c80
Yayehyrad <yayehyrad.sisay@gmail.com>
2024-07-11 23:34:31 +0300

    refactor: Remove unused font import in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,11 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+// import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/navbar/Navbar";
 import ActiveSectionContextProvider from "@/context/active-section-context";
 import { Toaster } from "react-hot-toast";
 
-const inter = Inter({ subsets: ["latin"] });
+// const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Yayehyrad | Personal Portfolio",
@@ -20,9 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="!scroll-smooth">
-      <body
-        className={`${inter.className} bg-bodyColor text-lightText relative`}
-      >
+      <body className="bg-bodyColor text-lightText relative">
         <div className=" absolute bg-gradient-to-b bg-backgroundImage from-[#212428] to-bodyColor w-[100vw] h-[100vh] top-0   -z-40"></div>
         {/* <ThemeContextProvider> */}
         <ActiveSectionContextProvider>
