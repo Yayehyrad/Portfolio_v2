13951083502479e3948baa465af270a52e9d7ad1
Yayehyrad <yayehyrad.sisay@gmail.com>
2024-07-14 01:13:27 +0300

    refactor: Update landing page with new images and fix unused imports

--- lib/data.ts
+++ lib/data.ts
@@ -2,9 +2,8 @@ import React from "react";
 import { CgWorkAlt } from "react-icons/cg";
 import { FaReact } from "react-icons/fa";
 import { LuGraduationCap } from "react-icons/lu";
-import corpcommentImg from "@/public/corpcomment.png";
-import rmtdevImg from "@/public/rmtdev.png";
-import wordanalyticsImg from "@/public/wordanalytics.png";
+import blue from "@/public/blue.png";
+import euphoria from "@/public/Euphoria.png";
 
 export const links = [
   {
@@ -85,26 +84,25 @@ export const educationsData = [
 
 export const projectsData = [
   {
-    title: "CorpComment",
-    description:
-      "I worked as a full-stack developer on this startup project for 2 years. Users can give public feedback to companies.",
-    tags: ["React", "Next.js", "MongoDB", "Tailwind", "Prisma"],
-    imageUrl: corpcommentImg,
+    title: "Blue Interior Desing ",
+    description: "I created this landing page for an interior design company.",
+    tags: ["React", "Tailwind", "Type Script"],
+    imageUrl: blue,
   },
   {
-    title: "rmtDev",
+    title: "Euphoria Hotel Management",
     description:
-      "Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.",
-    tags: ["React", "TypeScript", "Next.js", "Tailwind", "Redux"],
-    imageUrl: rmtdevImg,
+      "website for a hotel mangement purpose.It has features like filtering, sorting and pagination.",
+    tags: ["React", "TypeScript", "Node js", "Tailwind"],
+    imageUrl: euphoria,
   },
-  {
-    title: "Word Analytics",
-    description:
-      "A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
-    tags: ["React", "Next.js", "SQL", "Tailwind", "Framer"],
-    imageUrl: wordanalyticsImg,
-  },
+  // {
+  //   title: "Word Analytics",
+  //   description:
+  //     "A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
+  //   tags: ["React", "Next.js", "SQL", "Tailwind", "Framer"],
+  //   imageUrl: wordanalyticsImg,
+  // },
 ] as const;
 
 export const skillsData = [
